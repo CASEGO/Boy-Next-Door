0d6731ec58f2acc74c3e3e14277dc90803d06c4c
Sense <lspd07385@gmail.com>
2022-03-23 17:24:02 +0300

    Работа с формой заказы.

--- ARM Delivery/Form5.Designer.cs
+++ ARM Delivery/Form5.Designer.cs
@@ -30,16 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.aRMDataSet1 = new ARM_Delivery.ARMDataSet1();
-            this.заказыBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.заказыTableAdapter = new ARM_Delivery.ARMDataSet1TableAdapters.ЗаказыTableAdapter();
             this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -50,10 +40,21 @@
             this.dataGridViewTextBoxColumn17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.заказыBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.aRMDataSet1 = new ARM_Delivery.ARMDataSet1();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.заказыBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.заказыTableAdapter = new ARM_Delivery.ARMDataSet1TableAdapters.ЗаказыTableAdapter();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.aRMDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -78,84 +79,6 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(969, 342);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(627, 369);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Найти заказ";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(500, 372);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(627, 398);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Удалить заказ";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(397, 379);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Найти заказ";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(805, 398);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(174, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Выход в меню";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(807, 369);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(174, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Добавить заказ";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(724, 427);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(174, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Помощь";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // aRMDataSet1
-            // 
-            this.aRMDataSet1.DataSetName = "ARMDataSet1";
-            this.aRMDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // заказыBindingSource
-            // 
-            this.заказыBindingSource.DataMember = "Заказы";
-            this.заказыBindingSource.DataSource = this.aRMDataSet1;
-            // 
-            // заказыTableAdapter
-            // 
-            this.заказыTableAdapter.ClearBeforeFill = true;
             // 
             // dataGridViewTextBoxColumn10
             // 
@@ -225,11 +148,102 @@
             this.заказыBindingSource1.DataMember = "Заказы";
             this.заказыBindingSource1.DataSource = this.aRMDataSet1;
             // 
+            // aRMDataSet1
+            // 
+            this.aRMDataSet1.DataSetName = "ARMDataSet1";
+            this.aRMDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1000, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Найти заказ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1110, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1000, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Удалить заказ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(997, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите код";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1000, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Выход в меню";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1000, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(210, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Добавить заказ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1000, 165);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(210, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Помощь";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // заказыBindingSource
+            // 
+            this.заказыBindingSource.DataMember = "Заказы";
+            this.заказыBindingSource.DataSource = this.aRMDataSet1;
+            // 
+            // заказыTableAdapter
+            // 
+            this.заказыTableAdapter.ClearBeforeFill = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1000, 136);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(210, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "К списку";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(991, 450);
+            this.ClientSize = new System.Drawing.Size(1241, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
@@ -240,11 +254,13 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form5";
             this.Text = "Заказы";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form5_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form5_FormClosed);
             this.Load += new System.EventHandler(this.Form5_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.aRMDataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.заказыBindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -291,5 +307,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn17;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn18;
         private System.Windows.Forms.BindingSource заказыBindingSource1;
+        private System.Windows.Forms.Button button6;
     }
 }
--- ARM Delivery/Form5.cs
+++ ARM Delivery/Form5.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace ARM_Delivery
 {
     public partial class Form5 : Form
+        
     {
+        public static string connectString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= ARM.mdb";
+        private OleDbConnection myConnection;
         public Form5()
         {
             InitializeComponent();
@@ -19,6 +23,8 @@ namespace ARM_Delivery
         public Form5(Form1 f)
         {
             InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -42,8 +48,48 @@ namespace ARM_Delivery
 
         private void button5_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Я овощ - мне нужна помощь!", "Внимание!");
+            MessageBox.Show("Для поиска заказа введите код заказа и нажмите НАЙТИ ЗАКАЗ. Для Добовления заказа нажмите добавить заказ и заполните данные, затем нажмите ДОБАВИТЬ. Для удаления заказа введите код заказа и нажмите УДАЛИТЬ ЗАКАЗ. ", "Внимание!");
             return;
+        }
+
+        private void Form5_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Form5_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kod = Convert.ToInt32(textBox1.Text);
+            string query = "SELECT [Код заказа], [Номер заказа], [ФИО], [Дата доставки заказа], [Адрес заказа], [Номер телефона], [Блюдо], [Напиток], [Доставщик] FROM Заказы WHERE  [Код заказа] LIKE '%" + kod + "%' ";
+            OleDbDataAdapter command = new OleDbDataAdapter(query, myConnection);
+            DataTable dt = new DataTable();
+            command.Fill(dt);
+            dataGridView1.DataSource = dt;
+            myConnection.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+            dataGridView1.DataSource = заказыBindingSource;
+            this.заказыTableAdapter.Fill(this.aRMDataSet1.Заказы);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int kod = Convert.ToInt32(textBox1.Text);
+            string query = "DELETE FROM Заказы WHERE [Код заказа] = " + kod;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Данные обновлены!");
+            dataGridView1.DataSource = заказыBindingSource;
+            this.заказыTableAdapter.Fill(this.aRMDataSet1.Заказы);
         }
     }
 }
